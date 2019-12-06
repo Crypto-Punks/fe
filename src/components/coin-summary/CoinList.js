13e6dbe151c4cda9b56d471e4adafd6b0a2074dd
evanandrewson <evanandrewson@gmail.com>
2019-12-06 10:56:23 -0800

    made coin list component

---
+++ src/components/coin-summary/CoinList.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Coin from './Coin';
+
+const CoinList = ({ items }) => {
+  const elements = items.map(item => {
+    return <Coin key={item.name} item={item} />;
+  });
+  return (
+    <ul>
+      {elements}
+    </ul>
+  );
+};
+
+CoinList.propTypes = {
+  items: PropTypes.arrayOf(PropTypes.shape({
+    logo: PropTypes.string,
+    name: PropTypes.string.isRequired,
+    price: PropTypes.number.isRequired,
+    changePercent24Hr: PropTypes.number.isRequired
+  })).isRequired
+};
+
+export default CoinList;
