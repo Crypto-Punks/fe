7029946d34ce863bc382aa7ac46849ae00340c81
Pat Haynes <pathaynes32@gmail.com>
2019-12-10 10:15:28 -0800

    hamburger menu 80 percent there

--- src/components/ourAssets/AssetList.js
+++ src/components/ourAssets/AssetList.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import AssetItem from './AssetItem';
+import styles from './AssetList.css';
 
 const AssetList = ({ investedCoins }) => {
   const coins = investedCoins.map(coin => {
     return <AssetItem key={coin.name} coin={coin} />; 
   });
   return (
-    <ul>
+    <ul className={styles.AssetList}>
       {coins}
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
     </ul>
   );
 };
--- src/containers/SignOut.js
+++ src/containers/SignOut.js
@@ -2,12 +2,14 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { sessionSignOut } from '../actions/sessionActions';
+import exitIcon from '../images/exitIcon.png';
+import styles from './SignOut.css';
 
 
 
 function SignOut({ signOut }) {
-  return (  
-    <button onClick={() => signOut()}>Sign Out</button>
+  return (
+    <button className={styles.SignOut} onClick={() => signOut()}><img src={exitIcon} /></button>
   );
 }
 
