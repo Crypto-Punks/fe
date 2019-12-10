d2f539426f038b4ad16ad45db5d6bcd9b349ff54
Pat Haynes <pathaynes32@gmail.com>
2019-12-09 19:14:00 -0800

    got nav icon to work

--- src/components/hamburger-menu/NavMenu.js
+++ src/components/hamburger-menu/NavMenu.js
@@ -4,13 +4,14 @@ import { Link } from 'react-router-dom';
 import SignOut from '../../containers/SignOut';
 import styles from '../hamburger-menu/navMenu.css';
 import { getOpenMenu } from '../../selectors/menuSelectors';
+import Icon from '../../images/home.png';
 
 const NavMenu = () => {
   const openMenu = useSelector(getOpenMenu);
   return (
     <div className={`${styles.NavMenu} ${openMenu ? styles.open : styles.closed}`} >
       <nav>
-        <Link to='/home' className={styles.Home}><img></img></Link>
+        <Link to='/home'><img src={Icon} /></Link>
         <Link to='/portfolio'>Portfolio image</Link>
         <Link to='/transaction'>Transaction image</Link>
         <SignOut>Signout image</SignOut>
