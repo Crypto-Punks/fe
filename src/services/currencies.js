f3e98062dbb25e3a4644b1f69707dce08b0475cf
evanandrewson <evanandrewson@gmail.com>
2019-12-10 16:33:23 -0800

    added charts to coin detail page

--- src/services/currencies.js
+++ src/services/currencies.js
@@ -9,3 +9,4 @@ export const getTop100Currencies = () => get(CURRENCY_URL);
 export const getCoinById = (id) => get(`${CURRENCY_URL}/${id}`);
 export const getWatchList = () => get(`${CURRENCY_URL}/watched`);
 export const getInvestedList = () => get(`${CURRENCY_URL}/invested`);
+export const getPriceHistory = (id) => get(`${CURRENCY_URL}/history/${id}/d1`);
