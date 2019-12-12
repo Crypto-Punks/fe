85785a9194321c662ee4ac646a203ea087fa51bf
Joe Klause <joeklause@gmail.com>
2019-12-11 19:34:59 -0800

    slight refactor to trades fetch

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
 # fe
 1. menu button not visible... div slightly on screen at all times?
-2. transaction page, persist the form data because we may want to move around to visit other information sites
+2. radio buttons on coin details
+3. loading 
+4. search on select in transaction
+5. make math better in transaction
+6. charts on profile page
+7. about page
+8. tests
 
--- src/services/trades.js
+++ src/services/trades.js
@@ -3,8 +3,19 @@ export const TRADE_BASE_URL = 'http://localhost:7891/api/v1/trade';
 
 
 export const addTrade = (toCurrency, toCurrencyAmount, fromCurrency, fromCurrencyAmount, exchangeRate) => {
-  console.log(toCurrency, toCurrencyAmount, fromCurrency, fromCurrencyAmount, exchangeRate);
-  return post(`${TRADE_BASE_URL}`, { from_currency: { name: fromCurrency, amount: fromCurrencyAmount }, to_currency: { name: toCurrency, amount: toCurrencyAmount }, exchange_rate: exchangeRate })
+  const trade = { 
+    from_currency: { 
+      name: fromCurrency, 
+      amount: fromCurrencyAmount 
+    }, 
+    to_currency: { 
+      name: toCurrency, 
+      amount: toCurrencyAmount 
+    }, 
+    exchange_rate: exchangeRate 
+  };
+  
+  return post(`${TRADE_BASE_URL}`, trade)
     .then(result => {
       console.log(result);
     });
