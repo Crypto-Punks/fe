ce44ec4f4ce5e91b0e30d79daf054ecd9c7a4ae3
Pat Haynes <pathaynes32@gmail.com>
2019-12-12 16:09:14 -0800

    css update for about us

--- src/components/about-us/AboutUsItem.js
+++ src/components/about-us/AboutUsItem.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styles from '../about-us/AboutUs.css';
 
 const Person = ({ name, spiritAnimal, image, blurb, website }) => {
   return (
     <div>
       <h1>{name}</h1>
+      <div className={styles.HeadShot}>
+        <img src={image} alt={name} />
+        <p>{blurb}</p>
+      </div>
       <h2>Spirit Animal: {spiritAnimal}</h2>
-      <img src={image} alt={name}/>
       <a target='_blank' rel="noopener noreferrer" href={website}>{`${name}'s Website`}</a>
-      <p>{blurb}</p>
     </div>
   );
 };
--- src/components/about-us/AboutUsList.js
+++ src/components/about-us/AboutUsList.js
@@ -3,6 +3,7 @@ import { connect } from 'react-redux';
 import Developer from './AboutUsItem';
 import DeveloperMenu from '../hamburger-menu/DeveloperMenu';
 import { developerData } from '../../developerData';
+import styles from './AboutUs.css';
 
 const DeveloperList = () => {
   const developerElements = developerData.map((developer, i) => {
@@ -10,9 +11,11 @@ const DeveloperList = () => {
   });
   return (
     <>
-      <ul>
-        {developerElements}
-      </ul>
+      <div className={styles.AboutUs}>
+        <ul>
+          {developerElements}
+        </ul>
+      </div>
       <DeveloperMenu />
     </>
   );
